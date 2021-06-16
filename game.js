e39c20078e6f43d50c822fe4f558a73accc53f7e
eduardonk9999 <thiago.novak7@gmail.com>
2021-06-16 08:25:47 -0300

    Add, scripts

--- game.js
+++ game.js
@@ -10,38 +10,16 @@ const cardArray = [
 ]
 
 const grid = document.querySelector('.grid')
-let cardsChosen = []
-let cardsChosenId = []
-let cardsWon = []
 
-function createBorad() {
-  for (let i = 0; i < cardArray.length; i++) {
-    var card = document.createElement('img')
-    card.setAttribute('src', '../imgs/capas.png')
-    card.setAttribute('data-id', i)
-    card.addEventListener('click', flipcard)
-    grid.appendChild(card)
-  }
+for (let i = 0; i < cardArray.length; i++) {
+  let img = document.createElement('img')
+  img.setAttribute('src', cardArray[i].img)
+
+  grid.appendChild(img)
+
+  img.addEventListener('click', clickFlip)
 }
 
-
-//check for metches
-function checkForMatch() {
-
+function clickFlip() {
+  console.log('flip')
 }
-
-
-//flip card
-function flipcard() {
-  let cardId = this.getAttribute('data-id')
-  cardsChosen.push(cardArray[cardId].name)
-  cardsChosenId.push(cardId)
-  this.setAttribute('src', cardArray[cardId].img)
-
-  if (cardsChosen.length === 2) {
-    setTimeout(checkForMatch, 500)
-  }
-}
-
-
-createBorad()
