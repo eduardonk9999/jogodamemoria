5ec31b70055d871861d010c0e2d18533baba8b2c
eduardonk9999 <thiago.novak7@gmail.com>
2021-06-22 09:36:02 -0300

    Add, setup novo do projeto

--- game.js
+++ game.js
@@ -1,42 +1,33 @@
 const cardArray = [
   {
     name: 'frog',
-    img: 'frog.jpg'
+    img: 'frog.jpg',
   },
   {
     name: 'frog',
-    img: 'frog.jpg'
-  }
-]
+    img: 'frog.jpg',
+  },
+];
 
-const grid = document.querySelector('.grid')
-
-for (let i = 0; i < cardArray.length; i++) {
-  let img = document.createElement('img')
-  img.setAttribute('src', '../imgs/capas.png')
-  //cardArray[i].img
-
-  img.setAttribute('data-ids', i)
-
-  grid.appendChild(img)
-
-  img.addEventListener('click', clickFlip)
-}
+const grid = document.querySelector('.grid');
 
 function clickFlip() {
-  let imgData = this.dataset.ids
-
-
-
-
-  for (let j = 0; j < cardArray.length; j++) {
+  const imgData = this.dataset.ids;
+  for (let j = 0; j < cardArray.length; j += 1) {
     if (j === Number(imgData)) {
-      console.log('OK')
-      this.setAttribute('src', `../imgs/${cardArray[j].img}`)
-
+      console.log('OK');
+      this.setAttribute('src', `../imgs/${cardArray[j].img}`);
     } else {
-      console.log('Nao ok')
+      console.log('Nao ok');
     }
   }
+}
 
+for (let i = 0; i < cardArray.length; i += 1) {
+  const img = document.createElement('img');
+  img.setAttribute('src', '../imgs/capas.png');
+
+  img.setAttribute('data-ids', i);
+  grid.appendChild(img);
+  img.addEventListener('click', clickFlip);
 }
